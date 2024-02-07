bba76704865497a3762d8eb26333a655ad902430
Marcel Ferreira Campos <marcelfcampos@gmail.com>
2024-02-07 16:59:07 -0300

    1. Operadores Lógicos 2. Operações Matemáticas
    1. Operadores Lógicos
    2. Operações Matemáticas

---
+++ Operadores-Logicos-Operacoes- Matemáticas/src/OperacoeMatematicas.java
@@ -0,0 +1,66 @@
+public class OperacoeMatematicas {
+    public static void main(String[] args) {
+        int n = 49;
+        int m = 10;
+
+        int soma = n + m;
+        int subtracao = n - m;
+        int multiplicacao = n * m;
+        double divisao = n / m;
+        int resto = n % m;
+        // incremento de n
+        n++;
+//        n = n + 1;
+        int incremento = n;
+
+        // decremento  de m
+        m--;
+//            m = m - 1;
+        int decremento = m;
+
+
+        System.out.println("Soma de n + m");
+        System.out.println(soma);
+
+        System.out.println("subtracao de n - m");
+        System.out.println(subtracao);
+
+        System.out.println("multiplicacao de n * m");
+        System.out.println(multiplicacao);
+
+        System.out.println("divisao de n / m");
+        System.out.println(divisao);
+
+        System.out.println("resto de n % m");
+        System.out.println(resto);
+
+        System.out.println("incremento n++");
+        System.out.println(incremento);
+
+        System.out.println("incremento m--");
+        System.out.println(decremento);
+    }
+}
+
+        /*
+        + -> Adição
+        1 + 2
+        - -> Subtração
+        19 - 5
+        * -> Multiplicação
+        5 * 7
+        / -> Divisão
+        6 / 3
+        % -> Resto da Divisão
+        7 % 3
+        ++ -> Incremento
+        n++
+        -- -> Decremento
+        n--
+        */
+
+
+
+
+
+
---
+++ Primeiro Codigo/src/OperacoeMatematicas.java
@@ -0,0 +1,49 @@
+public class OperacoeMatematicas {
+    public static void main(String[] args) {
+        int n = 49;
+        int m = 10;
+
+        int soma = n + m;
+        int subtracao = n - m;
+        int multiplicaocao = n * m;
+        double divisao = n / m;
+        int resto = n % m;
+        // incremento de n
+        n++;
+        int incremento = n;
+        // decremento  de m
+        m--;
+        int decremento = m;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicaocao);
+        System.out.println(divisao);
+        System.out.println(resto);
+        System.out.println(incremento);
+        System.out.println(decremento);
+    }
+}
+
+        /*
+        + -> Adição
+        1 + 2
+        - -> Subtração
+        19 - 5
+        * -> Multiplicação
+        5 * 7
+        / -> Divisão
+        6 / 3
+        % -> Resto da Divisão
+        7 % 3
+        ++ -> Incremento
+        n++
+        -- -> Decremento
+        n--
+        */
+
+
+
+
+
+

256ba9a3f21869aab05edb1d3c37498115488eed
Marcel Ferreira Campos <marcelfcampos@gmail.com>
2024-02-07 16:11:45 -0300

    1. Primeiro Código 2. Operadores Lógicos 3.Operações Matemáticas 4. Sintaxe Básica
    1. Primeiro Código
    2. Operadores Lógicos
    3.Operações Matemáticas
    4. Sintaxe Básica

---
+++ 1-PrimeiroCodigo/src/OpeeradoresLogicos.java
@@ -0,0 +1,43 @@
+public class OpeeradoresLogicos {
+    public static void main(String[] args) {
+
+        int n = 49;
+        int m = 10;
+        boolean nMaiorQueM = (n > m);
+        boolean nMaiorOuIgualQueM = (n >= m);
+        boolean nMenorQueM = (n < m);
+        boolean nMenorOuIgualQueM = (n <= m);
+        boolean nIgualM = (n == m);
+        boolean nDiferenteM = (n != m);
+
+        System.out.println(nMaiorQueM);
+        System.out.println(nMaiorOuIgualQueM);
+        System.out.println(nMenorQueM);
+        System.out.println(nMenorOuIgualQueM);
+        System.out.println(nIgualM);
+        System.out.println(nDiferenteM);
+
+
+                    /*
+            > Maior que
+            n > m
+            >= Maior ou igual
+            n >= m
+            < Menor que
+            n < m
+            <= Menor ou igual
+            n <= m
+            == Igualdade
+            n == m
+            != Diferença
+            n != m
+            || Ou
+            n > m || n != o
+            && E
+            n > m && n != o
+            */
+
+
+
+    }
+}
---
+++ 1-PrimeiroCodigo/src/SintaxeBasica.java
@@ -0,0 +1,11 @@
+
+// Na linha abaixo declaramos uma nova classe pública com nome Main
+// O nome da classe sempre igua ao nome do arquivo .java
+public class SintaxeBasica {
+
+    // Na linha abaixo declaramos um método main responsável por tornar a nossa classe executável
+    public static void main(String[] args) {
+        // Na linha abaixo utilizamos uma função do Java escrever no console (terminal)
+        System.out.println("Hello word!!");
+    }
+}
